008cbf753327d6df859e011e461fa8ac44eb58be
Constanze Bayer <bayer.constanze@gmail.com>
2022-01-02 14:55:44 +0100

    Bar chart added

--- bond map.R
+++ bond map.R
@@ -1,5 +1,6 @@
 #https://d4tagirl.com/2017/05/how-to-plot-animated-maps-with-gganimate
 #https://www.datanovia.com/en/blog/how-to-create-a-map-using-ggplot2/ 
+#http://www.colorhunter.com/tag/jamesbond/9
 
 library('ggthemes')
 library(transformr)
@@ -25,11 +26,30 @@ bond <- bond %>%
     country_to_join == 'Hong Kong' ~ 'China',
     country_to_join == 'Macau' ~ 'China', 
     TRUE ~ country_to_join
-  )) #%>% 
-  # left_join(world_map, by = c('rstats_countries' = 'region'), keep = T)
+  )) 
+
+
+
+italy_importance <- bond %>% 
+  left_join(by = 'Bond', 
+            y = (bond %>% 
+              group_by(Bond) %>% 
+              select(Bond, Movie) %>%
+              unique() %>%
+              summarise(no_movies = n(), .groups = 'keep'))) %>% 
+  filter(Depicted_Film_Loc == 'Italy') %>% 
+  group_by(Bond) %>% 
+  mutate(italy_index = n()/no_movies) %>% 
+  select(Bond, italy_index)
+
+italy_importance %>% 
+  unique() %>% 
+  ggplot(aes(Bond, italy_index))+
+  geom_col()
 
 map_bond <- bond %>% 
-  group_by(Year, Movie, Depicted_Film_Loc, rstats_countries) %>% 
+  filter(Bond == 'Daniel Craig') %>% 
+  group_by(Year, Movie, Depicted_Film_Loc, rstats_countries, Bond) %>% 
   summarise(count_n = n(), .groups = 'keep') %>% 
   group_by(Depicted_Film_Loc) %>% 
   mutate(cumulated_appearance = sum(count_n))
@@ -38,6 +58,23 @@ world_joined <- world_map %>%
   left_join(map_bond, by = c('region' = 'rstats_countries'))
 
 names(world_joined)
+
+world_joined %>% 
+  ggplot(aes(long, lat, group = group)) +
+  geom_polygon(aes(fill = cumulated_appearance)) +
+  theme(
+    axis.title = element_blank(), 
+    panel.background = element_blank(), 
+    text = element_text(family = 'sans'),
+    axis.text = element_blank(),
+    axis.ticks = element_blank(),
+    legend.position = 'bottom'
+  ) +
+  ggtitle('How often does Bond visit a country?') +
+  scale_fill_gradient(NULL, 
+                      low = '#A17671', high = '#EB0000', na.value = '#C8C2CE')
+
+# Animation 
 
 m <- world_joined %>% 
   ggplot(aes(long, lat, group = group)) +
@@ -60,3 +97,4 @@ anim <- m +
   ggtitle('{closest_state}')
 
 animate(anim, end_pause = 10)
+
