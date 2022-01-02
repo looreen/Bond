d2ab6a6161ca1af60a4e59d081cd8afed0f04f2c
Constanze Bayer <bayer.constanze@gmail.com>
2022-01-02 14:22:07 +0100

    First attempt on a map

--- bond map.R
+++ bond map.R
@@ -1,22 +1,62 @@
-worldMap <- getMap()
-plot(worldMap)
+#https://d4tagirl.com/2017/05/how-to-plot-animated-maps-with-gganimate
+#https://www.datanovia.com/en/blog/how-to-create-a-map-using-ggplot2/ 
 
-countries <- worldMap$NAME %>% as_tibble()
-countries
+library('ggthemes')
+library(transformr)
+library(gganimate)
 
-bond %>% 
-  left_join(countries, by = c('country_to_join' = 'value'), keep = T)
+world <- ggplot() +
+  borders("world", colour = "gray85", fill = "gray80") +
+  theme_map() 
+
+world
+
+world_map <- map_data("world")
+
+bond %>% select(country_to_join) %>% head()
 
 bond <- bond %>% 
-  group_by(Movie, Year, Depicted_Film_Loc, country_to_join, Bond) %>% 
-  summarise(count_n = n(), .groups = 'keep')
+  mutate(rstats_countries = case_when(
+    country_to_join == 'United Kingdom' ~ 'UK',
+    country_to_join == 'United States' ~ 'USA',
+    country_to_join == 'S. Korea' ~ 'South Korea',
+    country_to_join == 'N. Korea' ~ 'North Korea', 
+    country_to_join == 'Czech Rep.' ~ 'Czech Republic',
+    country_to_join == 'Hong Kong' ~ 'China',
+    country_to_join == 'Macau' ~ 'China', 
+    TRUE ~ country_to_join
+  )) #%>% 
+  # left_join(world_map, by = c('rstats_countries' = 'region'), keep = T)
 
-bond$count_n
+map_bond <- bond %>% 
+  group_by(Year, Movie, Depicted_Film_Loc, rstats_countries) %>% 
+  summarise(count_n = n(), .groups = 'keep') %>% 
+  group_by(Depicted_Film_Loc) %>% 
+  mutate(cumulated_appearance = sum(count_n))
 
-sPDF <- joinCountryData2Map(bond, joinCode='NAME', nameJoinColumn='country_to_join')
-#somehow a problem with matching
+world_joined <- world_map %>% 
+  left_join(map_bond, by = c('region' = 'rstats_countries'))
 
-#default map (see rworldmap documentation for options e.g. catMethod, numCats, colourPalette, mapRegion)
-#missingCountryCol used for NA and countries not in the join file
-p <- mapCountryData(sPDF, nameColumnToPlot='count_n', missingCountryCol='dark grey', catMethod = 'pretty')
+names(world_joined)
 
+m <- world_joined %>% 
+  ggplot(aes(long, lat, group = group)) +
+  geom_polygon(aes(fill = count_n)) +#, color = 'white') +
+  theme(
+    axis.title = element_blank(), 
+    panel.background = element_blank(), 
+    text = element_text(family = 'sans'),
+    axis.text = element_blank(),
+    axis.ticks = element_blank(),
+    legend.position = 'bottom'
+  ) +
+  scale_fill_viridis_c(option = "C")
+
+m
+  
+anim <- m +
+  transition_states(Year, transition_length = 2, state_length = 2)+
+  shadow_mark(alpha = 0.5)+
+  ggtitle('{closest_state}')
+
+animate(anim, end_pause = 10)
